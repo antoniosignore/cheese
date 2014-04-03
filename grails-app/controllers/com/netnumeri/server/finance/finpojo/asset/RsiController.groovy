e5f458d5c81ac9ac6ec013b93f31756730c9fc0b
Antonio Signore <antonio.signore@gmail.com>
2014-04-03 23:17:04 +0200

    first commit

---
+++ grails-app/controllers/com/netnumeri/server/finance/finpojo/asset/RsiController.groovy
@@ -0,0 +1,48 @@
+package com.netnumeri.server.finance.finpojo.asset
+
+import com.netnumeri.server.finance.beans.TimeSeries
+import com.netnumeri.server.finance.strategy.SSASignal
+import com.netnumeri.server.finance.strategy.Strategy
+import com.netnumeri.server.finance.ta.Indicator
+import com.netnumeri.server.finance.ta.NormalizedSeriesIndicator
+import com.netnumeri.server.finance.ta.PriceChannelLowerIndicator
+import com.netnumeri.server.finance.ta.PriceChannelUpIndicator
+import com.netnumeri.server.finance.ta.RSIIndicator
+import com.netnumeri.server.finance.utils.DateUtils
+
+class RsiController {
+
+    def dailyService
+
+    static allowedMethods = [show: "GET", update: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "show", params: params)
+    }
+
+    def show() {
+
+        def stockInstance = Stock.get(params.id as Long)
+        if (!stockInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'stock.label', default: 'Stock'), params.id])
+            redirect(action: "show")
+            return
+        }
+
+        Date da = DateUtils.todayThreeMonthsAgo()
+        Date a = DateUtils.today()
+        dailyService.refreshStock(stockInstance, da, a)
+        TimeSeries closeSeries = stockInstance.buildCloseSeries()
+        stockInstance.indicators.put("normalized", new NormalizedSeriesIndicator(closeSeries, "Normalized"))
+        stockInstance.indicators.put("rsi", new RSIIndicator(closeSeries, "RSI",14))
+
+        Strategy strategy = new SSASignal("test", stockInstance, da, a);
+
+        [
+                startDate: da,
+                endDate: a,
+                stockInstance: stockInstance,
+                strategyInstance: strategy
+        ]
+    }
+}
--- grails-app/views/layouts/_header.gsp
+++ grails-app/views/layouts/_header.gsp
@@ -2,3 +2,5 @@
 <g:link controller="bb" id="${params.id}" role="button" class="btn btn-success btn-small">BB</g:link>
 <g:link controller="priceChannel" id="${params.id}" role="button" class="btn btn-success btn-small">PC</g:link>
 <g:link controller="macd" id="${params.id}" role="button" class="btn btn-success btn-small">MACD</g:link>
+<g:link controller="rsi" id="${params.id}" role="button" class="btn btn-success btn-small">RSI</g:link>
+<g:link controller="cci" id="${params.id}" role="button" class="btn btn-success btn-small">CCI</g:link>
